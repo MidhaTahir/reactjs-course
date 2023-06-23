59fdbd169db7df8b59bba508a7929d40d7055b1e
MidhaTahir <MidhaTahir>
2023-06-24 03:01:55 +0500

    add feedback done with contextApi

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Card from "./Card/Card";
 import FeedbackForm from "./FeedbackForm/FeedbackForm";
 import FormWithFormik from "./FeedbackForm/FormWithFormik";
+import FormWithFormikContextApi from "./FeedbackForm/FormWithFormikContextApi";
 // Exporting all the components from this file
-export { Card ,FeedbackForm,FormWithFormik};
+export { Card ,FeedbackForm,FormWithFormik,FormWithFormikContextApi};
