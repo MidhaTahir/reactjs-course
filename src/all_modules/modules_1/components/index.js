d54d1a1faf72f58bfa95711eed52bc63e86a1164
MidhaTahir <midhatahirkhan2011@gmail.com>
2023-07-08 02:14:02 +0500

    module 3 and 4 completed

---
+++ src/all_modules/modules_1/components/index.js
@@ -0,0 +1,7 @@
+import Card from "./Card/Card";
+import Counter from "./Counter/Counter";
+import FirstComponent from "./FirstComponent/FirstComponent";
+// import Feedback from "./Feedback/Feedback";
+import FeedbackForm from "./FeedbackForm/FeedbackForm";
+// Exporting all the components from this file
+export { Card, Counter, FirstComponent,FeedbackForm };
