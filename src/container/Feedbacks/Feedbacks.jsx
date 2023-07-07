dd002bf60ebd3bc11b98eaa7156cb7f0ed55f7a4
MidhaTahir <midhatahirkhan2011@gmail.com>
2023-07-08 00:15:37 +0500

    add feedback done with contextApi

---
+++ src/container/Feedbacks/Feedbacks.jsx
@@ -0,0 +1,31 @@
+import React, { useContext } from "react";
+import { Card } from "../../components";
+import { FormContext } from '../../context/FormContext';
+
+const Feedbacks = () => {
+  const { formInputs } = useContext(FormContext);
+console.log(formInputs);
+  return (
+    <>
+      <div className="all-cards">
+       
+        {
+          formInputs.length === 0 ? <h1 style={{textAlign:"center",color:"red"}}>No Feedbacks</h1> : 
+          formInputs.map((input) => {
+            return (
+              <Card
+                key={input.id}
+                title={input.title}
+                content={input.feedback}
+                rating={input.rating}
+              />
+            );
+          })
+        }
+        
+      </div>
+    </>
+  );
+};
+
+export default Feedbacks;
