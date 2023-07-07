b70e7f65ac8c6706d48d796001fc0045bf360e04
MidhaTahir <midhatahirkhan2011@gmail.com>
2023-07-08 02:14:02 +0500

    router v6

--- src/pages/FeedbackDetails.jsx
+++ src/pages/FeedbackDetails.jsx
@@ -1,21 +1,16 @@
 import React, { useContext } from "react";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 import { FormContext } from "../context/FormContext";
 
 const FeedbackDetails = () => {
-    const { feedbackId } = useParams();
-    const { getSingleCardData  } = useContext(FormContext);
-
-    const singleCardData = getSingleCardData(feedbackId);
+  const { feedbackId } = useParams();
 
   return (
     <>
-    <h1>Feedback Details</h1>
-    <h4>Title : <span>{singleCardData?.title}</span></h4>
-    <h4>Feedback : <span>{singleCardData?.feedback}</span></h4>
-    <h4>Rating : <span>{singleCardData?.rating}</span></h4>
+      <h1>Feedback Details</h1>
+      <h4>{feedbackId}</h4>
     </>
-  )
-}
+  );
+};
 
-export default FeedbackDetails
+export default FeedbackDetails;
