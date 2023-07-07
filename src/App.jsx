387901838e7e1051f6ba275e0c978ae99b38aa13
MidhaTahir <midhatahirkhan2011@gmail.com>
2023-07-08 02:14:02 +0500

    react router v5 not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
-// --- Importing Components using index.js --- //
-import { Card,FeedbackForm,FormWithFormik,FormWithFormikContextApi } from "./components";
-import "./App.css";
-import Feedbacks from "./container/Feedbacks/Feedbacks";
-import { FormProvider } from './context/FormContext';
 
+import Configuration from "./configuration/Configuration";
 
 function App() {
   return (
     <>
-     <FormProvider>
-      <FormWithFormikContextApi/>
-    {/* <FormWithFormik/> */}
-    {/* <FeedbackForm /> */}
-      <div className="feedbacks">
-        <h1 style={{ textAlign: "center" }}>Feedbacks</h1>
-       <Feedbacks/>
-      </div>
-      </FormProvider>
+     <Configuration/>
     </>
   );
 }
 
 export default App;
 
-/*
-Form Input & State
-Custom Button Component
-Real-Time Validation
-Rating Select Component
-Add Feedback
-Fade Animation With Framer Motion
-
-*/
---
+++ src/pages/index.jsx
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import About from "./About";
+import Feedback from "./Feedback";
+export { Home, About,Feedback };
